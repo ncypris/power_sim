c0672b081a124695d2aa61f460fc678df33b70ef
Nik <39830836+ncypris@users.noreply.github.com>
2020-09-14 16:01:31 +0200

    getting started with the adjustements. TUrns out, I didn't have a right solutions-file.

--- first_tutorial_solutions.R
+++ first_tutorial_solutions.R
@@ -1,3 +1,5 @@
+if (!require("tidyverse")) install.packages("tidyverse")
+if (!require("paramtest")) install.packages("paramtest")
 library(tidyverse)
 library(paramtest)
 
@@ -7,13 +9,13 @@ lm_sim <- function(simNum, N, b1,b0=0)
   
   con <- rnorm(N,0,1)
   rerror <- sqrt(1-b1^2)
-  y = b1*pol + 0 + rnorm(N,0,rerror)
-  model1 = lm(y ~ pol)
+  y = b1*con + 0 + rnorm(N,0,rerror)
+  model1 = lm(y ~ con)
   
   
   # model 1
-  est1_x1 <- coef(summary(model1))['pol', 'Estimate']
-  p1_x1 <- coef(summary(model1))['pol', 'Pr(>|t|)']
+  est1_x1 <- coef(summary(model1))['con', 'Estimate']
+  p1_x1 <- coef(summary(model1))['con', 'Pr(>|t|)']
   sig1_x1 <- p1_x1 < .05
   
   return(c(est1_x1 = est1_x1, p1_x1 = p1_x1, sig1_x1 = sig1_x1))
@@ -21,13 +23,13 @@ lm_sim <- function(simNum, N, b1,b0=0)
 }
 
 
-power_lm <- grid_search(lm_sim, params=list(N = c(50, 100, 150, 200, 250), b1 = c(.1, .2, .3)),
-                         n.iter = 1000, output ='data.frame', parallel = 'snow', ncpus = 4)
+power_lm <- grid_search(lm_sim, params=list(N = c(150, 200, 250)),
+                        n.iter = 1000, output ='data.frame', b1 = .2, parallel = 'snow', ncpus = 4)
 
 results(power_lm) %>%
-  group_by(N.test, b1.test) %>%
+  group_by(N.test) %>%
   summarise(
-    beta_pol = mean(est1_x1),
+    beta_con = mean(est1_x1),
     power = mean(sig1_x1)
   )
 
@@ -36,44 +38,20 @@ results(power_lm) %>%
 
 # 1. Vary the effect size of the b1-coefficient (.1, .2, .3) and the number of participants (50, 100, 150).
 
-# 2. Add another predictor to the equation. Adjust output (e.g. est1_x2 etc) and results table.
-country.origin <- rnorm(N,0,1)
-rerror <- sqrt(1-b1^2)
-y = b1*country.origin + 0 + rnorm(N,0,rerror)
-model2 = lm(y ~ country.origin)
-
-est1_x2 <- coef(summary(model2))['country.origin', 'Estimate']
-p1_x2 <- coef(summary(model2))['country.origin', 'Pr(>|t|)']
-sig1_x2 <- p1_x2 < .05
-
-power_lm <- grid_search(lm_sim, params=list(N = c(50, 100, 150, 200, 250), b1 = c(.1, .2, .3)),
+power_lm <- grid_search(lm_sim, params=list(N = c(50, 100, 150), b1 = c(.1, .2, .3)),
                         n.iter = 1000, output ='data.frame', parallel = 'snow', ncpus = 4)
 
-lm_sim2 <- function(simNum, N, b1,b0=0)
-{
-  
-  country.origin <- rnorm(N,0,1)
-  rerror <- sqrt(1-b1^2)
-  y = b1*country.origin + rnorm(N,0,rerror)
-  model2 = lm(y ~ country.origin)
-  
-  est1_x2 <- coef(summary(model2))['country.origin', 'Estimate']
-  p1_x2 <- coef(summary(model2))['country.origin', 'Pr(>|t|)']
-  sig1_x2 <- p1_x2 < .05
-  
-  return(c(est1_x2 = est1_x2, p1_x2 = p1_x2, sig1_x2 = sig1_x2))
-  
-}
-
-power_lm2 <- grid_search(lm_sim2, params=list(N = c(50, 100, 150, 200, 250), b1 = c(.1, .2, .3)),
-                        n.iter = 1000, output ='data.frame', parallel = 'snow', ncpus = 4)
-
-results(power_lm2) %>%
+results(power_lm) %>%
   group_by(N.test, b1.test) %>%
   summarise(
-    beta_country.origin = mean(est1_x2),
-    power = mean(sig1_x2)
-)
+    beta_con = mean(est1_x1),
+    power = mean(sig1_x1)
+  )
+
+# 2. Add another predictor to the equation. Adjust output (e.g. est1_x2 etc) and results table.
+
+
+
 # 3. Compare model1 with one predictor (b1 = .05) and model2 with two predictors (b1 = .05, b2 = .2).
 
 
