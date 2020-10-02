269e7c3012d6cd052e28d49b342a335e9d755263
Daniel Toribio-Flórez <toribio-florez@coll.mpg.de>
2020-10-02 16:28:32 +0200

    updated log_mlm simulation file

--- ballafoutas_withinsubject_logmlm.R
+++ ballafoutas_withinsubject_logmlm.R
@@ -1,3 +1,14 @@
+bin_data <- as.data.frame(genCorGen(
+  n = 134,
+  nvars = 6,
+  params1 = c(.35,.3,.25,.15,.10,.05),
+  dist = "binary",
+  rho = .5,
+  corstr = "cs" #see below.
+))
+
+test
+
 log_sim <- function(simNum,N,t,rho){
   library(simstudy)
   library(lme4)
@@ -6,30 +17,34 @@ log_sim <- function(simNum,N,t,rho){
     genCorGen(
       n = N,
       nvars = t,
-      params1 = c(.5,.3,.1),
+      params1 = c(.35,.3,.25,.15,.10,.05),
       dist = "binary",
       rho = rho,
       corstr = "cs" #see below.
     ))
   
-  bin_data$period <- as.factor(bin_data$period)
-  mod <- glmer(X~period+(1|id), data = bin_data,
+  
+  bin_data$effic <- ifelse(bin_data$period == 0 | bin_data$period == 3, "Low",
+                          ifelse(bin_data$period == 1 | bin_data$period == 4, "Med",
+                                 "High"))
+  bin_data$cond <- ifelse(bin_data$period == 0 | bin_data$period == 1 | bin_data$period == 2, "APUN",
+                          "CPUN")
+  mod <- glmer(X~cond+(1|id)+(1|effic), data = bin_data,
                family = binomial(link = "logit"))
   summary(mod)
   p_val1 <- summary(mod)$coefficients[2,4] #Extract p-value for 3rd factor from outcome.
-  p_val2 <- summary(mod)$coefficients[3,4]
+ 
   
   sig1 <- p_val1 < .05 #Returns TRUE if p-value < alpha.
-  sig2 <- p_val2 < .05 #Returns TRUE if p-value < alpha.
-  return(c(p_val1 = p_val1, sig1 = sig1,
-           p_val2 = p_val2, sig2 = sig2))
+
+  return(c(p_val1 = p_val1, sig1 = sig1))
 }
 
 set.seed(1234)
 power_log <- grid_search(log_sim,
                          params = list(N = 134,
-                                       t = 3,
-                                       rho = c(.5,.7,.9)),
+                                       t = 6,
+                                       rho = .5),
                          n.iter = 1000,
                          output = "data.frame",
                          parallel = "snow",
@@ -37,7 +52,7 @@ power_log <- grid_search(log_sim,
                          beep = 1)
 
 results(power_log) %>%
-  group_by(rho.test) %>%
+  # group_by(rho.test) %>%
   summarise(
     power1 = mean(sig1),
     power2 = mean(sig2)
