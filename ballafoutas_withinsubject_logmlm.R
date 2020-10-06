d78daf2d882d3630146996b0f164fcc575f6a47b
Nik <39830836+ncypris@users.noreply.github.com>
2020-10-06 17:48:46 +0200

    used rep() function for quicker variable creation added relevant packages included sanity checks for effect sizes (had a bad experience with a package that did not do what it was supposed to do here once)

--- ballafoutas_withinsubject_logmlm.R
+++ ballafoutas_withinsubject_logmlm.R
@@ -1,6 +1,8 @@
+library(tidyverse)
 library(simstudy)
 library(lme4)
 library(lmerTest)
+library(paramtest)
 
 bin_data <- as.data.frame(genCorGen(
   n = 134,
@@ -10,8 +12,6 @@ bin_data <- as.data.frame(genCorGen(
   rho = .5,
   corstr = "cs" #see below.
 ))
-
-test
 
 log_sim <- function(simNum,N,t,rho){
   
@@ -25,22 +25,28 @@ log_sim <- function(simNum,N,t,rho){
       corstr = "cs" #see below.
     ))
   
+  first_mean <- bin_data %>% group_by(id) %>% slice_head() %>% ungroup() %>% summarise(mean = mean(X)) # sanity check: did assigning mean work
+  last_mean <- bin_data %>% group_by(id) %>% slice_tail() %>% ungroup() %>% summarise(mean = mean(X)) # sanity check: did assigning mean work
   
-  bin_data$effic <- ifelse(bin_data$period == 0 | bin_data$period == 3, "Low",
-                          ifelse(bin_data$period == 1 | bin_data$period == 4, "Med",
-                                 "High"))
-  bin_data$cond <- ifelse(bin_data$period == 0 | bin_data$period == 1 | bin_data$period == 2, "APUN",
-                          "CPUN")
+  bin_data$effic <- rep(c("low", "med", "high"), times = N*2) # because we have two conditions, CPUN & APUN, per participant with low, medium, and high efficiency
+  
+  # bin_data$effic <- ifelse(bin_data$period == 0 | bin_data$period == 3, "Low",
+  #                         ifelse(bin_data$period == 1 | bin_data$period == 4, "Med",
+  #                                "High"))
+  bin_data$cond <- rep(c("APUN", "CPUN"), each = 3, times = N)
+  
+  # bin_data$cond <- ifelse(bin_data$period == 0 | bin_data$period == 1 | bin_data$period == 2, "APUN",
+  #                         "CPUN")
   mod <- lme4::glmer(X~cond+(1|id)+(1|effic), data = bin_data,
                family = binomial(link = "logit"))
   
-  summary(mod)
+  # summary(mod)
   
   p_val1 <- summary(mod)$coefficients[2,4] #Extract p-value for 3rd factor from outcome.
  
   sig1 <- p_val1 < .05 #Returns TRUE if p-value < alpha.
 
-  return(c(p_val1 = p_val1, sig1 = sig1))
+  return(c(p_val1 = p_val1, sig1 = sig1, first_mean = first_mean[[1]], last_mean = last_mean[[1]]))
 }
 
 set.seed(1234)
@@ -57,8 +63,7 @@ power_log <- grid_search(log_sim,
 results(power_log) %>%
   # group_by(rho.test) %>%
   summarise(
-    power1 = mean(sig1),
-    power2 = mean(sig2)
+    power1 = mean(sig1)
   )
 
 power_log2 <- grid_search(log_sim,
