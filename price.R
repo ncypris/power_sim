bc1ff05bceadffdbb8d9f3e9256abf5d0faa91ec
Nik <39830836+ncypris@users.noreply.github.com>
2020-10-08 16:50:07 +0200

    flexible N

--- price.R
+++ price.R
@@ -1,6 +1,7 @@
 # price
+N = 600
 # Player A (take) + Player A (notake) + Player B (notake) + Player B (take/nopun) + Player C (nopun) + Player C (pun/cpun) + Player C (pun/nocpun)
-price <- .35*1.5*200 + .65*4.5*200  + 200*.35*6 + .5*.65*200*8.5 + 100*11.25 + 35 * 4.5 + 65 * 10.75
+price <- .35*1.5*N/3 + .65*4.5*N/3  + .35*6**N/3 + .5*.65*8.5**N/3 + .5*11.25*N/3 + .5*.35*4.5*N/3 + .5*.65*10.75*N/3
 price
 
 # Price pP
