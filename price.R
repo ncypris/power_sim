b6fc188834546645931138d18da20e28cccd321b
Nik <39830836+ncypris@users.noreply.github.com>
2020-10-08 16:30:39 +0200

    rough price estimation

---
+++ price.R
@@ -0,0 +1,3 @@
+# price
+# Player A (take) + Player A (notake) + Player B (notake) + Player B (take/nopun) + Player C (nopun) + Player C (pun/cpun) + Player C (pun/nocpun)
+.35*1.5*200 + .65*4.5*200  + 200*.35*6 + .5*.65*200*8.5 + 100*11.25 + 35 * 4.5 + 65 * 10.75
