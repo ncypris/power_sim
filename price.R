c4688261b6737f5ede0a2a9331b2762cc65df000
Nik <39830836+ncypris@users.noreply.github.com>
2020-10-08 16:42:30 +0200

    extend estimate

--- price.R
+++ price.R
@@ -1,3 +1,7 @@
 # price
 # Player A (take) + Player A (notake) + Player B (notake) + Player B (take/nopun) + Player C (nopun) + Player C (pun/cpun) + Player C (pun/nocpun)
-.35*1.5*200 + .65*4.5*200  + 200*.35*6 + .5*.65*200*8.5 + 100*11.25 + 35 * 4.5 + 65 * 10.75
+price <- .35*1.5*200 + .65*4.5*200  + 200*.35*6 + .5*.65*200*8.5 + 100*11.25 + 35 * 4.5 + 65 * 10.75
+price
+
+# Price pP
+price/6
